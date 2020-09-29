37b1211e3fc063832b35628dfcbd5a4863509a69
Andrés Felipe Chaparro Rosas <36827424+AFelipeCR@users.noreply.github.com>
2020-09-29 13:51:21 -0500

    Add files via upload

---
+++ src/test/java/co/edu/uptc/swii/microservicios/app/eureka/MicroservicioEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.swii.microservicios.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
